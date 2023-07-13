e7f6cd45dd4c50fc5d9df1cbc34836a91bb780b2
Laoset <xlaospt2@gmail.com>
2023-07-13 14:52:22 -0300

    withcors

--- index.js
+++ index.js
@@ -4,32 +4,22 @@ require("dotenv").config();
 
 const { conn } = require("./database.js");
 app.use(express.json());
-
+// Middleware de CORS
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://deploy-perritos.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 const bookRouter = require("./routes/dogsAndTemperaments.router");
 
 app.use("/", bookRouter);
 
-// app.listen(process.env.PORT, () =>
-//   console.log("Server is running on port 5000")
-// );
-
-// Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   app.listen(process.env.PORT, () => {
     console.log("Server is running on port 5000"); // eslint-disable-line no-console
   });
 });
-// const express = require("express");
-// const app = express();
-
-// require("dotenv").config();
-
-// app.use(express.json());
-
-// const bookRouter = require("./routes/dogsAndTemperaments.router");
-
-// app.use("/", bookRouter);
-
-// app.listen(process.env.PORT, () =>
-//   console.log("Server is running on port 5000")
-// );
