ae624461b30c43f796c4f1308661bbf74145b5e6
Laoset <xlaospt2@gmail.com>
2023-07-15 11:33:13 -0300

    f

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
+    "http://localhost:5000",
     "https://deploy-perritos.vercel.app"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
